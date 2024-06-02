a7b9b67ab675d40a1fc67f8f4d9ab29e8055340c
barkha <barkhaarora11107.n.BA@gmail.com>
2024-06-03 00:22:03 +0530

    Coming soon

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Countdown from 'react-countdown';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h2 className="app">CLOSEAI</h2>
+        <h1 className="coming"><span id="col">COMING</span></h1>
+        
+        <h1 className="coming"><span id="col">SOON</span></h1>
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
